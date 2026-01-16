9b5be9495702eb7b5c6ddea91a1b235a2ba2c251
dayana-navida <dayananavida@gmail.com>
2026-01-16 22:29:21 +0530

    Create dimensions_exploration.sql

---
+++ dimensions_exploration.sql
@@ -0,0 +1,6 @@
+---Explore All countries our customers come from 
+SELECT DISTINCT country FROM gold.dim_customers
+  
+---Explore All categories "The Major Divisions"
+SELECT DISTINCT category,subcategory,product_name FROM gold.dim_products
+ORDER BY 1,2,3
